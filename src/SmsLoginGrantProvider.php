802fd5a21616a81e57ec4a66ad908fe463df7d0a
Mohammadreza Kadivar <me.kadivar@gmail.com>
2019-09-22 13:19:57 +0330

    Initial commit.

---
+++ src/SmsLoginGrantProvider.php
@@ -0,0 +1,51 @@
+<?php
+namespace Kadivar\Passport\Sms;
+
+use Laravel\Passport\Bridge\RefreshTokenRepository;
+use Laravel\Passport\Bridge\UserRepository;
+use Laravel\Passport\Passport;
+use Laravel\Passport\PassportServiceProvider;
+use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Grant\PasswordGrant;
+
+/**
+ * Class SmsLoginGrantProvider
+ *
+ * @author Mohammadreza Kadivar <me.kadivar@gmail.com>
+ */
+class SmsLoginGrantProvider extends PassportServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        app(AuthorizationServer::class)->enableGrantType($this->makeSmsRequestGrant(), Passport::tokensExpireIn());
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Create and configure a Password grant instance.
+     *
+     * @return PasswordGrant
+     */
+    protected function makeSmsRequestGrant()
+    {
+        $grant = new SmsRequestGrant(
+            $this->app->make(UserRepository::class),
+            $this->app->make(RefreshTokenRepository::class)
+        );
+        $grant->setRefreshTokenTTL(Passport::refreshTokensExpireIn());
+        return $grant;
+    }
+}
